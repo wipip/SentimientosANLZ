1759eac22796a234cbebf77a9aa8da03ec3ca41e
wipip <166628878+wipip@users.noreply.github.com>
2026-03-12 07:05:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 from googletrans import Translator
+from streamlit_lottie import st_lottie
+import json
 
 st.title('Análizador de Sentimientos en Texto')
 image = Image.open('emoticones.jpg')
@@ -35,6 +37,9 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊 :)')
+            with open ('Sample Animation.json') as source:
+              animation=json.load(source)
+            st.lottie(animation, width= 350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔 :(')
         else:
